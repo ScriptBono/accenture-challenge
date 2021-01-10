3166f67afb856845b582132332ba75e9cf8c61d2
Bono <jonast@fantasymail.de>
2021-01-10 18:29:46 +0100

    added readme

--- Backend/app.py
+++ Backend/app.py
@@ -8,7 +8,6 @@ import pandas as pd
 from OpenAqDataHandler import OpenAqDataHandler
 
 DEBUG = True
-
 app = Flask(__name__)
 app.register_blueprint(datapoints_api, url_prefix='/api/v1')
 CORS(app, support_credentials=True)
